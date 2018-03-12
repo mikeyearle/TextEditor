9c056e37ae95ac963cd93ae9eaed335a7560668d
Lawrence Thorpe <thorpe_lawrence@yahoo.co.uk>
2018-03-12 22:59:10 +0000

    Initial electron-webpack React setup

---
+++ src/renderer/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../components/app';
+import './style.css';
+import 'github-markdown-css/github-markdown.css';
+
+ReactDOM.render(<App />, document.getElementById('app'));
