9ae487956f5d8fe31be5b630796c8fe0c9c8671b
Lawrence Thorpe <thorpe_lawrence@yahoo.co.uk>
2018-03-13 14:26:57 +0000

    Cleanup comments

--- src/main/index.js
+++ src/main/index.js
@@ -2,22 +2,21 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
 
-//Check is it development
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 let win;
-//Creates electron window
+
 function createWindow() {
     win = new BrowserWindow({
         width: 800,
         height: 600
     });
-    //Allows it to run in development mode
+    
     if (isDevelopment) {
+        // run from webpack-dev-server and open dev tools
         win.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
         win.webContents.openDevTools();
     }
-    //Live editing
     else {
         win.loadURL(url.format({
             pathname: path.resolve('index.html'),
